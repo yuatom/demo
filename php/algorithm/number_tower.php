7a5463459c9594e5504e0d7e55d17608354d43e8
Yuatom <yuatom93@gmail.com>
2016-07-28 12:03:03 +0800

    number_towe by php

--- php/algorithm/number_tower.php
+++ php/algorithm/number_tower.php
@@ -10,3 +10,31 @@
 
 例子中的最优方案是：5 + 8 + 6 + 9 = 28
 */
+function cal($input, $depth = 0, $index = 0, $sum = 0, $path = []){
+	static $max = 0;
+	static $maxPath = [];
+	if($depth >= count($input)){
+		if($sum > $max){
+			$maxPath = $path;
+			$max = $sum;
+		}
+	}else{
+		$sum += $input[$depth][$index];
+		array_push($path,[$depth, $index]);
+
+		cal($input, $depth + 1, $index, $sum, $path);
+		cal($input, $depth + 1, $index + 1, $sum, $path);
+	}
+	return [$max, $maxPath];
+}
+
+
+$input = [[5], [8, 4], [3, 6, 9], [7, 2, 9, 5]];
+$res = cal($input);
+//var_dump($res);exit;
+echo "<pre>";
+var_dump($res[0]);
+foreach ($res[1] as $key => $value) {
+	echo $value[0] , ',' , $value[1];
+	echo "<br>";
+}
